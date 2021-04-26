7c4f9d9cd054a0ed983a4d6daec9d51cfddb0da2
Kenji Hashimoto <k-hasimt@i.nagoya-u.ac.jp>
2021-04-26 14:19:39 +0900

    change files for building

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir -p build
+mkdir -p bin
+cd build
+
+if [ $1 = "d" ]; then
+  echo "Create Makefile for Debug..."
+  cmake -DCMAKE_BUILD_TYPE=Debug .. \
+  && make \
+  && cp gpmc ../bin/ 
+
+elif [ $1 = "r" ]; then
+  echo "Create Makefile for Release..."
+  cmake -DCMAKE_BUILD_TYPE=Release .. \
+  && make \
+  && cp gpmc ../bin/ 
+
+elif [ $1 = "clean" ]; then
+  make clean
+
+fi
+
