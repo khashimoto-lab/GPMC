9aff866ab35c0c69e4caf8c1e1f1dd5f03d9124f
Kenji Hashimoto <k-hasimt@i.nagoya-u.ac.jp>
2022-07-11 18:12:25 +0900

    update files for building

--- build.sh
+++ build.sh
@@ -7,17 +7,22 @@ cd build
 if [ $1 = "d" ]; then
   echo "Create Makefile for Debug..."
   cmake -DCMAKE_BUILD_TYPE=Debug .. \
-  && make \
+  && make -j2\
   && cp -p gpmc ../bin/ 
 
 elif [ $1 = "r" ]; then
   echo "Create Makefile for Release..."
   cmake -DCMAKE_BUILD_TYPE=Release .. \
-  && make \
+  && make -j2\
+  && cp -p gpmc ../bin/ 
+
+elif [ $1 = "rs" ]; then
+  echo "Create Makefile for Release Static..."
+  cmake -DCMAKE_BUILD_TYPE=Release -DGPMC_STATIC_BUILD=on .. \
+  && make -j2\
   && cp -p gpmc ../bin/ 
 
 elif [ $1 = "clean" ]; then
   make clean
 
 fi
-
