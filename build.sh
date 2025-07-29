c9ef7a768ec429700529ec012dce6711ebc3d599
Kenji Hashimoto <hashimoto.kenji@kagawa-u.ac.jp>
2025-07-29 18:23:10 +0900

    update CMakeLists.txt

--- build.sh
+++ build.sh
@@ -6,23 +6,20 @@ cd build
 
 if [ $1 = "d" ]; then
   echo "Create Makefile for Debug..."
-  cmake -DCMAKE_BUILD_TYPE=Debug .. \
-  && make -j2\
+  cmake -DCMAKE_BUILD_TYPE=Debug -DGPMC_USE_STATIC_LIBS=off -DGPMC_STATIC_BUILD=off .. \
+  && make -j$(nproc)\
   && cp -p gpmc ../bin/ 
 
 elif [ $1 = "r" ]; then
   echo "Create Makefile for Release..."
-  cmake -DCMAKE_BUILD_TYPE=Release .. \
-  && make -j2\
+  cmake -DCMAKE_BUILD_TYPE=Release -DGPMC_USE_STATIC_LIBS=off -DGPMC_STATIC_BUILD=off .. \
+  && make -j$(nproc)\
   && cp -p gpmc ../bin/ 
 
 elif [ $1 = "rs" ]; then
   echo "Create Makefile for Release Static..."
-  cmake -DCMAKE_BUILD_TYPE=Release -DGPMC_STATIC_BUILD=on .. \
-  && make -j2\
-  && cp -p gpmc ../bin/ 
-
-elif [ $1 = "clean" ]; then
-  make clean
-
+  cmake -DCMAKE_BUILD_TYPE=Release -DGPMC_USE_STATIC_LIBS=on -DGPMC_STATIC_BUILD=on .. \
+  && make -j$(nproc)\
+  && cp -p gpmc ../bin/
+   
 fi
